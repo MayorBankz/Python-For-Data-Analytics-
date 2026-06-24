aa05f0a57b64174c10c79149f8f9bf4721b9ab04
Idowu Mayowa <108333874+MayorBankz@users.noreply.github.com>
2026-06-24 16:52:41 +0100

    Add list functions practice file

--- 009-Data-Structure/list[] functions practice.py
+++ 009-Data-Structure/list[] functions practice.py
@@ -22,6 +22,7 @@ print(colors[0:3])
 print(colors[-2:])
 print(colors[1:])
 
+# Level 3
 # Given: animals = ['cat', 'dog', 'rabbit', 'goat']
 # perform the following
 # change the dog to lion
@@ -119,4 +120,73 @@ for num in numbers:
         largest = num
 print("Largest:", largest)
     
+# Level 9: Accessing Nested List
+# print:
+# Ada
+# 92
+# Mary
 
+student = [
+    ['Ada', 85],
+    ['John', 92],
+    ['Mary', 78]
+    ]
+
+print(student[0][0])
+print(student[1][1])
+print(student[2][0])
+
+# Level 10: List Unpacking
+# use list unpacking to store the values in"name, age, role"
+# Then print all three variables
+
+person = ['Mayowa', 26, "Data Analyst"]
+
+name, age, role = person
+
+print(name)
+print(age)
+print(role)
+
+# Level 11: Skipping items with "_"
+# Use unpacking and "_" to 
+# store the name in "name"
+# skip the age
+# store the role in role
+# print name and role
+
+person = ['Mayowa', 26, "Data Analyst"]
+
+name, _, role = person
+
+print(name)
+print(role)
+
+# Level 12: Exploring and Analyzing lists
+# Explore the lists: sales = [150, 200, 120, 300, 250]
+# print number of sales record
+# total sales
+# Highest sale
+# lowest sale
+
+sales = [150, 200, 120, 300, 250]
+
+print("Records:", len(sales))
+print("Total Sales:", sum(sales))
+print("Highest Sale:", max(sales))
+print("Lowest Sale:", min(sales))
+
+
+# Level 13: Advanced List Unpacking (*)
+# Task: Use unpacking to store:
+# First country -- First
+# Last country -- Last
+# All countries in between -- others 
+
+countries = ['Nigeria', 'Ghana', 'Kenya', 'South Africa', 'Egypt']
+
+first, *others, last = countries
+
+print(first)
+print(others)
+print(last)
