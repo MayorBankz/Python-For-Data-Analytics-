d7f1460d8f03715bc7595f4512fa86e63f32132b
Idowu Mayowa <108333874+MayorBankz@users.noreply.github.com>
2026-07-01 14:38:24 +0100

    Add files via upload

---
+++ 001-print() Function-&-Variables/print and variable functions practice.py
@@ -0,0 +1,127 @@
+# Task 1 
+# create a variable called name and store your first name in iter
+
+name = 'Mayowa'
+print(name)
+
+# Task 2: Create three variables:
+# first_name  -- Your first name
+# last_name -- Your last name
+# country -- your country
+# Then print each variable on a separate line
+
+first_name = 'Mayowa'
+last_name = 'Idowu'
+country = 'Nigeria'
+
+print(first_name)
+print(last_name)
+print(country)
+
+# Task 3: Create the following variables 
+# school = "University of Lagos"
+# age = 25
+# height = 1.75
+# The print in this exact order:
+# school
+# Age 
+# height
+
+school = 'University of Lagos'
+age = 25
+height = 1.75
+
+print(school)
+print(age)
+print(height)
+
+city = "Lagos"
+
+# Task 4: variables referencing variables)
+# Create the following variables
+# name = 'Mayowa'
+# friend = name
+# Then print:
+# name 
+# friend
+
+name = 'Mayowa'
+friend = name
+
+print(name)
+print(friend)
+
+# Task 5: Create these variables 
+# company = 'OpenAI'
+# city = 'Lagos'
+# year = 2026
+# Then print them in this exact format:
+# company: OpenAI
+# City: Lagos
+# Year: 2026
+
+company = 'OpenAI'
+city = 'Lagos'
+year = 2026
+
+print(f"Company: {company}")
+print(f"City: {city}")
+print(f"Year: {year}")
+
+# Task 6: Variables referencing Variables
+# Without using f strings, create the following Variables
+# language = 'Python'
+# favorite_language = language
+
+language = 'Python'
+favorite_language = language
+
+print("Language:", language)
+print("Favorite Language:", favorite_language)
+
+# Task 9
+# Create the following variables:
+# first_name = 'Mayowa'
+# last_name = 'Idowu'
+# age = 25
+# print the output exactly like this
+# Student Information
+# -------------------
+# First Name: Mayowa
+# Last Name: Idowu
+# Age: 25
+
+first_name = 'Mayowa'
+last_name = 'Idowu'
+age = 25
+
+print("Student Information")
+print('-' * 19)
+print("First name:", first_name)
+print("Last Name:", last_name)
+print("Age:", age)
+
+# Task 10: Create the following variables:
+# company = 'Swift Bank'
+# role = 'Python Developer'
+# salary = 850000
+# print the output exactly like this:
+# ==============================
+#        EMPLOYEE DETAILS
+# ==============================
+# Company : Swift Bank
+# Role    : Python Developer
+# Salary  : 850000
+# ==============================
+
+company = 'Swift Bank'
+role = 'Python Developer'
+salary = 850000
+
+print('=' * 30)
+print('EMPLOYEE DETAILS'.center(30))
+print('=' * 30)
+print("Company :", company)
+print("Role    :", role)
+print('Salary  :', salary)
+print('=' * 30)
