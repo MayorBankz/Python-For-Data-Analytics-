7ebadf0dd1ce7c3ebc8a91fe12a486cbc853cd87
Idowu Mayowa <108333874+MayorBankz@users.noreply.github.com>
2026-07-02 16:30:12 +0100

    Add files via upload

---
+++ 003-Data-Types/data type practice.py
@@ -0,0 +1,59 @@
+# Task 1: Create the variable: name = 'Mayowa'
+# Then print its data type using the type() function
+
+name = 'Mayowa'
+print(type(name))
+
+# Task 2: Create the variable: age = 25
+# Then print its data type
+
+age = 25
+print(type(age))
+
+# Task 3: float
+# Create the variable: height = 1.75
+# Then print its data type
+
+height = 1.75
+print(type(height))
+
+# task 4: Boolean
+# Create the variable: is_student = True
+# Then print its data type
+
+is_student = True
+print(type(is_student))
+
+# Task 5: Mixed Data type
+# create these variables: 
+# name = 'Mayowa'
+# age = 25
+# height = 1.75
+# is_student = True
+# Then print the data type of each variables
+
+name = 'Mayowa'
+age = 25
+height = 1.75
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+# Task 6: Create these variables
+# name = 'Mayowa'
+# age = 25
+# height = 1.75
+# is_employed = False
+# print each variable followed by it's data type
+name = 'Mayowa'
+age = 25
+height = 1.75
+is_employed = False
+
+print(name, type(age))
+print(age, type(age))
+print(height, type(height))
+print(is_employed, type(is_employed))
