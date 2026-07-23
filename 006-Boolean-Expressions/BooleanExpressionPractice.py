6aaada7eacd5f26f9dcf9aa8f5874da9778ffd2f
Idowu Mayowa <108333874+MayorBankz@users.noreply.github.com>
2026-07-23 08:49:36 +0100

    Add files via upload

---
+++ 006-Boolean-Expressions/BooleanExpressionPractice.py
@@ -0,0 +1,119 @@
+# Task 1: Given
+
+age = 25
+
+# Print the result of the following expressions
+# age > 18
+# age < 18
+
+print( age > 18)
+print(age < 18)
+
+# Task 2: Given
+
+username = "Mayowa"
+
+# Print the result of these expressions
+# username == "Mayowa"
+# username == "mayowa"
+# username != "John"
+
+print(username == "Mayowa")
+print(username == "mayowa")
+print(username != "John")
+
+# Task 3: Given
+
+score = 75
+
+# print the result of these expressions
+# score >= 70
+# score <= 60
+# score >= 75
+
+print(score >= 70)
+print(score <= 60)
+print(score >= 75)
+
+# Task 4: Given
+
+age = 25
+has_id = True
+
+# Print the result of these expressions
+# age >= 18 and has_id
+# age < 18 and has_id
+
+print(age >= 18 and has_id)
+print(age < 18 and has_id)
+
+# Task 5: Given
+
+is_weekend = False
+is_holiday = True
+
+# print the result of these expressions
+# is_weekend or is_holiday
+# False or False
+# True or False
+
+print(is_weekend or is_holiday)
+print(False or False)
+print(True or False)
+
+# Task 6: Given 
+
+is_logged_in = False
+
+# Print the result of these expressions:
+# not is_logged_in
+# not True
+# not False
+
+print(not is_logged_in)
+print(not True)
+print(not False)
+
+# Task 7: Given
+
+age = 22
+has_license = True
+has_car = False
+
+# Print the result of these expressions
+# age >= 18 and has_license
+# has_license and has_car
+# has_license or has_car
+# not has_car
+
+print( age >= 18 and has_license)
+print(has_license and has_car)
+print(has_license or has_car)
+print(not has_car)
+
+# Task 8: Given
+
+courses = ["Python", "SQL", "HTML", "CSS"]
+
+# Print the result of these expressions
+# "Python" in courses
+# "Java" in courses
+# "SQL" in courses
+
+print("Python" in courses)
+print("Java" in courses)
+print("SQL" in courses)
+
+# Task 9: not in
+# Given:
+
+fruits = ["Apple", "Banana", "Orange", "Mango"]
+
+# Print the results of these expressions
+# "Grape" not in fruits
+# "Apple" not in fruits
+# "Orange" in fruits
+
+print("Grape" not in fruits)
+print("Apple" not in fruits)
+print("Orange" in fruits)
